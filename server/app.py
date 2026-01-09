b7d026f09791cbb0e5df79674fb1afd365e8f825
Collins Kibet <kollcibe05@gmail.com>
2026-01-09 06:29:06 +0300

    UD code along

--- server/app.py
+++ server/app.py
@@ -79,7 +79,19 @@ class NewsletterByID(Resource):
         )
 
         return response
-
+    def patch(self, id):
+        record = Newsletter.query.get(id)
+        for attr in request.form:
+            setattr(record, attr, request.form[attr])
+        db.session.add(record)
+        db.session.commit()
+        response_dict = record.to_dict()
+        
+        response = make_response(
+        response_dict, 
+        200
+        )       
+        return response
 api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 
